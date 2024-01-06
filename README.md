0122b9ed1763267d8e4c14f8294569d55c7ee144
Seigo0adachi <145352812+Seigo0adachi@users.noreply.github.com>
2024-01-06 22:14:31 +0900

    課題提出２回目
    課題提出２回目

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
 
 
-https://vite-project-19fe1.web.app  課題提出url
+https://vite-project-19fe1.web.app 課題提出url
---
+++ components/chat/Send.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const update = (value) => {
+  const messageObj = {
+    // displayName: "自分さん", → 削除
+    message: value,
+  }
+  emit('sendMessage', messageObj)
+}
+const emit = defineEmits(['sendMessage']);
+const input = ref('');
+const doSend = () => {
+  if (input.value === '') return;
+  update(input.value);
+  input.value = '';
+};
+</script>
+
+<template>
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="メッセージを入力してください"
+      v-model="input"
+    />
+    <button
+      class="btn btn-outline-secondary js-send-message"
+      type="button"
+      @click="doSend"
+    >
+      メッセージ送信
+    </button>
+  </div>
+</template>
